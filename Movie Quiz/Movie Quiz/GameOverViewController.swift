9133162951460e7d27d21f862c1fb50c56a72605
vloss <viniciusvianaloss@gmail.com>
2023-02-17 16:31:39 -0300

    Finalização de elementos de views e criação de class para GameOver.

---
+++ Movie Quiz/Movie Quiz/GameOverViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameOverViewController.swift
+//  Movie Quiz
+//
+//  Created by Vinicius Loss on 17/02/23.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
