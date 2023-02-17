7f7fe34aa766ff572cb79bc9ced696776465839f
vloss <viniciusvianaloss@gmail.com>
2023-02-17 15:07:24 -0300

    Criação de projeto e inclusão de files disponibilizados no curso.

---
+++ Movie Quiz/Movie Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Quiz
+//
+//  Created by Vinicius Loss on 17/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
