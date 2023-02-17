d049e22cfdbbc303ddcfe72c8937196bd0b9d85c
vloss <viniciusvianaloss@gmail.com>
2023-02-17 16:43:08 -0300

    Criação de outlets e actions para elementos da view.

--- Movie Quiz/Movie Quiz/GameOverViewController.swift
+++ Movie Quiz/Movie Quiz/GameOverViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class GameOverViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playAgain(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
-
 }
--- Movie Quiz/Movie Quiz/ViewController.swift
+++ Movie Quiz/Movie Quiz/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viSoundBar: UIView!
+    @IBOutlet weak var slMusic: UISlider!
+    @IBOutlet var btOptions: [UIButton]!
+    @IBOutlet weak var ivQuiz: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showHideSoundBar(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeMusicStatus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func changeMusicTime(_ sender: UISlider) {
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+    }
 }
 
